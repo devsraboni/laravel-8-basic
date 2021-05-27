296ca00fe5d41d7c9b30e01491da6ec3fd34d1a8
sraboni303 <sraboni391@gmail.com>
2021-05-27 22:45:31 +0600

    edit & update by query builder

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -36,18 +36,23 @@ class CategoryController extends Controller
 
     public function edit($id)
     {
-        $category = Category::find($id);
+        // $category = Category::find($id);
+        $category = DB::table('categories')->where('id', $id)->first();
         return view('admin.category.edit', compact('category'));
     }
 
 
     public function update(Request $request, $id)
     {
-        $category = Category::find($id)->update([
-            'category_name' => $request->name,
-            'user_id' => Auth::user()->id,
-        ]);
+        // $category = Category::find($id)->update([
+        //     'category_name' => $request->name,
+        //     'user_id' => Auth::user()->id,
+        // ]);
 
+        $category = array();
+        $category['category_name'] = $request->name;
+        $category['user_id'] = Auth::user()->id;
+        DB::table('categories')->where('id', $id)->update($category);
         return redirect()->route('category.index')->with('success', 'Category has Updated Successfully!!');
     }
 }
